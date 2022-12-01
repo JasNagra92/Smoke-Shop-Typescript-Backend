fda99b70cd40196897458f145c6abe4e3ff8c73e
jasmac <jaskarannagra@hotmail.com>
2022-11-30 22:05:17 -0800

    add current user to request object by declaring global can now access current user from req object

---
+++ src/features/auth/interfaces/auth.interface.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: AuthPayload;
+    }
+  }
+}
+
+export interface AuthPayload {
+  userId: string;
+  email: string;
+  username: string;
+  iat?: number;
+}
