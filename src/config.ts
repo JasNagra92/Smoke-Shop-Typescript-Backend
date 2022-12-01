3d5da5067fabbf7320136fc1a18fb23c1bc50569
jasmac <jaskarannagra@hotmail.com>
2022-11-30 22:05:50 -0800

    config object responsible for storing and verifying env variables

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,22 @@
+import dotenv from 'dotenv';
+
+dotenv.config({});
+
 class Config {
   MONGO_URL: string | undefined;
   NODE_ENV: string | undefined;
   KEY_ONE: string | undefined;
   KEY_TWO: string | undefined;
+  JWT_SECRET: string | undefined;
+  CLIENT_URL: string | undefined;
 
   constructor() {
     this.MONGO_URL = process.env.MONGO_URL;
     this.KEY_ONE = process.env.KEY_ONE;
     this.KEY_TWO = process.env.KEY_TWO;
     this.NODE_ENV = process.env.NODE_ENV;
+    this.JWT_SECRET = process.env.JWT_SECRET;
+    this.CLIENT_URL = process.env.CLIENT_URL;
   }
   public validateConfig(): void {
     for (const [key, value] of Object.entries(this)) {
