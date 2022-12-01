a90df4752b531ea55f8336a0124839de11fa4257
jasmac <jaskarannagra@hotmail.com>
2022-11-30 22:04:53 -0800

    routes class that will use the different route classes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
+import { authRoutes } from './features/auth/routes/auth.routes';
 import { Application } from 'express';
 
-const BASE_PATH = '/api/vi';
+const BASE_PATH = '/api/v1';
 
 export default (app: Application) => {
-  app.use(BASE_PATH);
+  app.use(BASE_PATH, authRoutes.routes());
+  app.use(BASE_PATH, authRoutes.signOutRoute());
 };
