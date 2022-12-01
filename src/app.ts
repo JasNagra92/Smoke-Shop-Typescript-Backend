923740d5edf8e7317bd0d2df07c1ff0b68b7afd3
jasmac <jaskarannagra@hotmail.com>
2022-11-30 22:06:40 -0800

    add security middleware to server class as well as import and use database connection method in main app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import { smokeShopServer } from './setupServer';
 import express, { Express, Application } from 'express';
 import { config } from './config';
+import dataBaseConnect from './setupDatabase';
 
 class App {
   public initialize(): void {
     this.loadConfig();
+    dataBaseConnect();
     const app: Express = express();
     smokeShopServer.start(app);
   }
--- src/setupServer.ts
+++ src/setupServer.ts
@@ -1,10 +1,12 @@
-import express, { Application } from 'express';
+import express, { Application, json, urlencoded, Request, Response, NextFunction } from 'express';
 import hpp from 'hpp';
 import helmet from 'helmet';
 import http from 'http';
 import cors from 'cors';
 import compression from 'compression';
 import cookieSession from 'cookie-session';
+import appRoutes from './routes';
+import { config } from './config';
 
 const PORT = 4000;
 
@@ -24,14 +26,31 @@ class SmokeShopServer {
   }
 
   private securityMiddelware(app: Application): void {
+    app.use(
+      cookieSession({
+        name: 'session',
+        keys: [config.KEY_ONE!, config.KEY_TWO!],
+        maxAge: 24 * 7 * 3600000,
+        secure: config.NODE_ENV !== 'development'
+      })
+    );
     app.use(hpp());
     app.use(helmet());
-    app.use(cors());
-    app.use(compression());
-    app.use(cookieSession());
+    app.use(
+      cors({
+        origin: config.CLIENT_URL,
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+      })
+    );
   }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: '50mb' }));
+    app.use(urlencoded({ extended: true, limit: '50mb' }));
+  }
 
   // anonymous function that will be called from routes file
   private routesMiddleware(app: Application): void {
@@ -39,7 +58,12 @@ class SmokeShopServer {
   }
 
   // middleware that will be called after routes middleware to respond with error to client
-  private globalErrorHandler(app: Application): void {}
+  private globalErrorHandler(app: Application): void {
+    app.all('*', (req: Request, res: Response, next: NextFunction) => {
+      res.status(404).json({ message: `${req.originalUrl} not found` });
+      next();
+    });
+  }
 
   // creates http server and uses private method to start listening on port
   private async startServer(app: Application): Promise<void> {
