c8a6929184188d06f188269219589e372c53cf6f
jasmac <jaskarannagra@hotmail.com>
2022-12-09 08:10:25 -0800

    add password controller in charge of sending forgot password email method as well as method to update password, and auth services class in charge of using password tokens to find user by token

---
+++ src/features/auth/controllers/password.ts
@@ -0,0 +1,45 @@
+import { authServices } from './../../../shared/services/db/auth.services';
+import HTTP_STATUS from 'http-status-codes';
+import { BadRequestError } from './../../../shared/globals/helpers/errorHandler';
+import { userServices } from './../../../shared/services/db/user.services';
+import { Request, Response } from 'express';
+import crypto from 'crypto';
+
+export class Password {
+  public async forgotPassword(req: Request, res: Response): Promise<void> {
+    const { email } = req.body;
+
+    const user = await userServices.getUserByUsernameOrEmail('danny', email);
+
+    if (!user) {
+      throw new BadRequestError('No Email matches entered email');
+    }
+
+    const randomBytes: Buffer = await Promise.resolve(crypto.randomBytes(20));
+    const randomCharacters: string = randomBytes.toString('hex');
+
+    await authServices.setResetToken(email, randomCharacters);
+
+    res.status(HTTP_STATUS.OK).json({ message: 'reset email sent' });
+  }
+
+  public async resetPassword(req: Request, res: Response): Promise<void> {
+    const { password, confirmPassword } = req.body;
+    const { token } = req.params;
+
+    if (password !== confirmPassword) {
+      throw new BadRequestError('passwords must match');
+    }
+
+    const existingUser = await authServices.findUserByResetToken(token);
+
+    if (!existingUser) {
+      throw new BadRequestError('Reset token has expired');
+    }
+
+    existingUser.password = password;
+    await existingUser.save();
+
+    res.status(HTTP_STATUS.OK).json({ message: 'password updated successfully' });
+  }
+}
---
+++ src/shared/services/db/auth.services.ts
@@ -0,0 +1,18 @@
+import { IUserDocument } from 'src/features/user/interfaces/user.interface';
+import { userModel } from 'src/features/user/models/user.model';
+
+export class AuthServices {
+  public async setResetToken(email: string, token: string): Promise<void> {
+    const user = await userModel.findOne({ email }).exec();
+
+    user!.passwordResetToken = token;
+    user!.passwordResetExpires = Date.now() * 60 * 60 * 1000;
+    user!.save();
+  }
+  public async findUserByResetToken(token: string): Promise<IUserDocument | null> {
+    const user = await userModel.findOne({ passwordResetToken: token, passwordResetExpires: { $gt: Date.now() } }).exec();
+    return user;
+  }
+}
+
+export const authServices: AuthServices = new AuthServices();
