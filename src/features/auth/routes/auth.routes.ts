6e5d8fa18d2a10fc065812257cabe6c222f84157
jasmac <jaskarannagra@hotmail.com>
2022-12-09 08:12:42 -0800

    add 2 new routes for add reset token to user as well as updating password with new password

--- src/features/auth/routes/auth.routes.ts
+++ src/features/auth/routes/auth.routes.ts
@@ -2,6 +2,7 @@ import express, { Router } from 'express';
 import { SignUp } from '../controllers/signup';
 import { SignOut } from '../controllers/signout';
 import { SignIn } from '../controllers/signin';
+import { Password } from '../controllers/password';
 
 class AuthRoutes {
   private router: Router;
@@ -11,6 +12,8 @@ class AuthRoutes {
   public routes(): Router {
     this.router.post('/signup', SignUp.prototype.create);
     this.router.post('/signin', SignIn.prototype.verify);
+    this.router.post('/forgotPassword', Password.prototype.forgotPassword);
+    this.router.post('/resetPassword/:token', Password.prototype.resetPassword);
 
     return this.router;
   }
