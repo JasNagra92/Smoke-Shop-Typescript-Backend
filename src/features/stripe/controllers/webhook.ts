f3d7987c412a316d466270694928512e75b2ad4f
jasmac <jaskarannagra@hotmail.com>
2022-12-28 19:57:28 -0800

    create stripe controller and routes to handle creating checkout session and a webhook route to catch put request from stripe with checkout information

---
+++ src/features/stripe/controllers/webhook.ts
@@ -0,0 +1,63 @@
+/// <reference types="stripe-event-types" />
+import { emailQueue } from './../../../shared/services/queue/email.queue';
+import { orderQueue } from './../../../shared/services/queue/order.queue';
+/* eslint-disable no-unsafe-optional-chaining */
+import { Request, Response } from 'express';
+import { config } from 'src/config';
+import Logger from 'bunyan';
+import Stripe from 'stripe';
+import { Helpers } from 'src/shared/globals/helpers/helpers';
+import { formatInTimeZone } from 'date-fns-tz';
+
+const endpointSecret: string = config.STRIPE_ENDPOINT_SECRET!;
+
+const log: Logger = config.createLogger('webhookLog');
+
+const stripe: Stripe = new Stripe(config.STRIPE_API_KEY!, {
+  apiVersion: '2022-11-15'
+});
+
+export class Webhook {
+  public async handleSuccessfulPayment(req: Request, res: Response): Promise<void> {
+    const payload = req.body;
+    const sig = req.headers['stripe-signature'];
+
+    let event;
+
+    try {
+      event = stripe.webhooks.constructEvent(payload, sig!, endpointSecret) as Stripe.DiscriminatedEvent;
+    } catch (err: any) {
+      log.error('error creating stripe event');
+      res.status(400).json({ message: `Webhook Error: ${err.message}` });
+    }
+
+    if (event?.type === 'checkout.session.completed') {
+      const { id, amount_total } = event.data.object;
+      const { name, email } = event.data.object.customer_details as Stripe.Checkout.Session.CustomerDetails;
+      const pickupDate = event.data.object.metadata;
+
+      const sessionWithLineItems = await stripe.checkout.sessions.retrieve(id, {
+        expand: ['line_items']
+      });
+      const lineItems = sessionWithLineItems.line_items;
+
+      const orderNumber = await Helpers.createOrderNumber();
+      const today = formatInTimeZone(new Date(Date.now()), 'Canada/Pacific', 'MM/dd/yyyy H:mm');
+      orderQueue.addOrderJob('addOrderToDB', {
+        checkoutSessionId: id,
+        name,
+        email,
+        amount_total,
+        pickupDate: `${pickupDate!.pickupDate}`,
+        items: lineItems?.data,
+        orderNumber,
+        orderDate: today
+      });
+
+      res.status(200).end();
+    } else {
+      log.info(event?.type);
+      res.status(200).end();
+    }
+  }
+}
