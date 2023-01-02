00fe73eafd4a5ffa809d2e33a66477202494d61b
jasmac <jaskarannagra@hotmail.com>
2023-01-01 20:12:43 -0800

    checkout session that is created on buy now button

--- src/features/stripe/controllers/createCheckoutSession.ts
+++ src/features/stripe/controllers/createCheckoutSession.ts
@@ -1,5 +1,9 @@
+import { orderServices } from './../../../shared/services/db/order.services';
+import { BadRequestError } from './../../../shared/globals/helpers/errorHandler';
+import { IOrderItemFrontEnd, IOrderItemBackend } from './../../cart/interfaces/order.interface';
+import { menuItemService } from './../../../shared/services/db/menu.services';
 import HTTP_STATUS from 'http-status-codes';
-import { IOrderPayload, IOrderItem } from './../../order/interfaces/order.interface';
+import { IOrderPayload } from '../../cart/interfaces/order.interface';
 import { Request, Response } from 'express';
 import Stripe from 'stripe';
 import { config } from 'src/config';
@@ -14,9 +18,24 @@ const stripe: Stripe = new Stripe(config.STRIPE_API_KEY!, {
 export class CheckoutSession {
   public async create(req: Request, res: Response): Promise<void> {
     const { payload }: { payload: IOrderPayload } = req.body;
-    const { items }: { items: IOrderItem[] } = payload;
-    const line_items = [];
+    const { items }: { items: IOrderItemFrontEnd[] } = payload;
+
+    const query = [];
     for (const item of items) {
+      query.push(item._id);
+    }
+
+    const stockError = await menuItemService.compareOrderToInventoryStock(query, items);
+
+    if (stockError) {
+      log.error('not enough stock');
+      throw new BadRequestError('not enough stock to complete order');
+    }
+
+    const orderedItems: IOrderItemBackend[] = await orderServices.createOrderedItems(items);
+
+    const line_items = [];
+    for (const item of orderedItems) {
       line_items.push({
         price: item.price_id,
         quantity: item.quantity
