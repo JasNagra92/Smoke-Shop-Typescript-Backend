e7d8f816cf1541482bed04a50477355a513b9f93
jasmac <jaskarannagra@hotmail.com>
2023-01-07 19:19:14 -0800

    move tags into resource

--- deployment/8-elastic_ips.tf
+++ deployment/8-elastic_ips.tf
@@ -2,9 +2,10 @@ resource "aws_eip" "elastic_ip" {
   depends_on = [
     aws_internet_gateway.main_igw
   ]
+
+  tags = merge(
+    local.common_tags,
+    tomap({ "Name" = "${local.prefix}-eip" })
+  )
 }
 
-tags = merge(
-  local.common_tags,
-  tomap({ "Name" = "${local.prefix}-eip" })
-)
