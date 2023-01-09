ade80c72f4f1d08b1b6565f661947be0c8ca4515
jasmac <jaskarannagra@hotmail.com>
2023-01-08 21:24:39 -0800

    add build to ignore and fix typo in start script, app.js location

--- build/routes.js
+++ build/routes.js
@@ -1,2 +1,25 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const healthRoutes_1 = require("./features/health/routes/healthRoutes");
+const stripeRoutes_1 = require("./features/stripe/routes/stripeRoutes");
+const orderRoutes_1 = require("./features/cart/routes/orderRoutes");
+const base_queue_1 = require("./../src/shared/services/queue/base.queue");
+const authMiddleware_1 = require("./shared/globals/helpers/authMiddleware");
+const currentUser_routes_1 = require("./../src/features/auth/routes/currentUser.routes");
+const auth_routes_1 = require("./../src/features/auth/routes/auth.routes");
+const menuRoutes_1 = require("./features/menu/routes/menuRoutes");
+const BASE_PATH = '/api/v1';
+exports.default = (app) => {
+    app.use('/health', healthRoutes_1.healthRoutes.healthRoute());
+    app.use('/instance', healthRoutes_1.healthRoutes.instance());
+    app.use('/env', healthRoutes_1.healthRoutes.env());
+    app.use('/queues', base_queue_1.serverAdapter.getRouter());
+    app.use('/webhook', stripeRoutes_1.stripeRoutes.webhookRoutes());
+    app.use(BASE_PATH, auth_routes_1.authRoutes.routes());
+    app.use(BASE_PATH, auth_routes_1.authRoutes.signOutRoute());
+    app.use(BASE_PATH, menuRoutes_1.menuRoutes.routes());
+    app.use(BASE_PATH, orderRoutes_1.cartRoutes.routes());
+    app.use(BASE_PATH, stripeRoutes_1.stripeRoutes.routes());
+    app.use(BASE_PATH, authMiddleware_1.authMiddleware.verifyUser, currentUser_routes_1.currentUserRouter.routes());
+};
 //# sourceMappingURL=routes.js.map
--- build/shared/services/db/user.services.js
+++ build/shared/services/db/user.services.js
@@ -1,2 +1,55 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.userServices = void 0;
+const errorHandler_1 = require("./../../globals/helpers/errorHandler");
+const user_model_1 = require("../../../features/user/models/user.model");
+const validator_1 = __importDefault(require("validator"));
+class UserServices {
+    addUserToDB(data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield user_model_1.userModel.create(data);
+        });
+    }
+    findUserByUsername(username) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const foundUser = yield user_model_1.userModel.findOne({ username });
+            return foundUser;
+        });
+    }
+    getUserByUsernameOrEmail(username, email) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const query = {
+                $or: [{ username }, { email }]
+            };
+            const user = (yield user_model_1.userModel.findOne(query).exec());
+            return user;
+        });
+    }
+    validateCredentials(email, password, phoneNumber) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!validator_1.default.isEmail(email)) {
+                throw new errorHandler_1.ValidationError('email must be valid');
+            }
+            if (!validator_1.default.isStrongPassword(password)) {
+                throw new errorHandler_1.ValidationError('password not strong enough');
+            }
+            if (!validator_1.default.isMobilePhone(phoneNumber)) {
+                throw new errorHandler_1.ValidationError('phone number must be a phone number');
+            }
+        });
+    }
+}
+exports.userServices = new UserServices();
 //# sourceMappingURL=user.services.js.map
