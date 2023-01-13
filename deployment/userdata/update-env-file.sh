db9ca6c86170ab015de9e8908f85ea1a75266a52
jasmac <jaskarannagra@hotmail.com>
2023-01-12 20:02:45 -0800

    update env file script for new s3 bucket layout

--- deployment/userdata/update-env-file.sh
+++ deployment/userdata/update-env-file.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-aws s3 sync s3://smoke-server-env-files/develop .
+aws s3 sync s3://smoke-server-env-files/backend/develop .
 unzip env-file.zip
 cp .env.develop .env
 rm .env.develop
@@ -8,6 +8,6 @@ sed -i -e "s|\(^REDIS_HOST=\).*|REDIS_HOST=redis://$ELASTICACHE_ENDPOINT:6379|g"
 rm -rf env-file.zip
 cp .env .env.develop
 zip env-file.zip .env.develop
-aws --region us-west-2 s3 cp env-file.zip s3://smoke-server-env-files/develop/
+aws --region us-west-2 s3 cp env-file.zip s3://smoke-server-env-files/backend/develop/
 rm -rf .env*
 rm -rf .env-file.zip
--- src/features/health/routes/healthRoutes.ts
+++ src/features/health/routes/healthRoutes.ts
@@ -12,9 +12,7 @@ class HealthRoutes {
 
   public healthRoute(): Router {
     this.router.get('/health', (req: Request, res: Response) => {
-      res
-        .status(HTTP_STATUS.OK)
-        .send(`Health: Server instance is healthy with process id ${process.pid} on ${moment().format('LL')}, launched with github`);
+      res.status(HTTP_STATUS.OK).send(`Health: Server instance is healthy with process id ${process.pid} on ${moment().format('LL')}`);
     });
 
     return this.router;
